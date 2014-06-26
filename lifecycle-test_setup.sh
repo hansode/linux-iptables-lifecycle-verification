d7fa68793513035d99510333ede1039e44e121fd
Masahito Yoshida <masahito@axsh.net>
2014-06-26 19:10:52 +0900

    add generate_iptables_config.

--- lifecycle-test_setup.sh
+++ lifecycle-test_setup.sh
@@ -6,6 +6,7 @@
 
 node=node01
 
-generate_iptables_rule ${node}
-show_iptables_rule     ${node}
-force_stop_iptables    ${node}
+generate_iptables_rule   ${node}
+generate_iptables_config ${node}
+show_iptables_rule       ${node}
+force_stop_iptables      ${node}
