3a11e36bb5f5b580f348f6bab97d45e417b6732d
Masahito Yoshida <masahito@axsh.net>
2014-06-27 21:07:20 +0900

    compare previous rule to current rule.

--- lifecycle-test_26.sh
+++ lifecycle-test_26.sh
@@ -22,6 +22,19 @@ start_iptables ${node}
 
 generate_iptables_rule2 ${node}
 
-show_iptables_rule_counters ${node}
-            reload_iptables ${node}
-show_iptables_rule_counters ${node}
+current_rule="$(show_iptables_rule_counters ${node})"
+previous_rule="${current_rule}"
+
+set +x
+
+for i in {1..5}; do
+  echo "... i=${i}"
+
+  reload_iptables ${node}
+  current_rule="$(show_iptables_rule_counters ${node})"
+
+  # should not be same
+  ! diff <(echo "${previous_rule}") <(echo "${current_rule}")
+
+  previous_rule="${current_rule}"
+done
