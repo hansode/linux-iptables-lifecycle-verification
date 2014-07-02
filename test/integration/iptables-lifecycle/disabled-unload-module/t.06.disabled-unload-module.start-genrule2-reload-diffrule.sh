94de68cfeeb6b5ba1e939afb716519dc66199e55
Masahito Yoshida <masahito@axsh.net>
2014-07-03 02:02:52 +0900

    add necessary loop.

--- test/integration/iptables-lifecycle/disabled-unload-module/t.06.disabled-unload-module.start-genrule2-reload-diffrule.sh
+++ test/integration/iptables-lifecycle/disabled-unload-module/t.06.disabled-unload-module.start-genrule2-reload-diffrule.sh
@@ -22,18 +22,26 @@ function test_disable_unload_start_genrule2_cmprule() {
   [[ -n "${before_str}" ]]
   assertEquals "should show iptables rule and counters" 0 ${?}
 
-  reload_iptables ${node}
-  assertEquals "should reload iptables" 0 ${?}
+  for i in {1..3}; do
+    reload_iptables ${node}
+    assertEquals "should reload iptables" 0 ${?}
 
-  run_in_target ${node} "hostname" >/dev/null
-  assertEquals "should grow packet counter" 0 ${?}
+    j=0
+    while [[ ${i} != ${j} ]]; do
+      run_in_target ${node} "hostname" >/dev/null
+      assertEquals "should grow packet counter" 0 ${?}
+      j=$((${j} + 1))
+    done
 
-  after_str="$(show_iptables_rule_counters ${node})"
-  [[ -n "${after_str}" ]]
-  assertEquals "should show iptables rule counters" 0 ${?}
+    after_str="$(show_iptables_rule_counters ${node})"
+    [[ -n "${after_str}" ]]
+    assertEquals "should show iptables rule counters" 0 ${?}
 
-  diff_str "${before_str}" "${after_str}"
-  assertNotEquals "should have difference" 0 ${?}
+    diff_str "${before_str}" "${after_str}"
+    assertNotEquals "should have difference" 0 ${?}
+
+    before_str="${after_str}"
+  done
 }
 
 ## shunit2
